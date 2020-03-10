5f00fae919acea7be340cb21261ee7918b6d602b
Eudrey <eudreybandelaria@gmail.com>
2020-03-10 17:30:23 +0100

    added sign-in and sign-up page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+// ANGULAR
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// COMPONENTS
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+
+// PAGES
 import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CreateArticlePageComponent } from './pages/create-article-page/create-article-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ArticleListComponent } from './components/article-list/article-list.component';
+
+const appRoutes: Routes = [
+  { path: 'sign-in', component: SignInPageComponent },
+  { path: 'sign-up', component: SignUpPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'create-article', component: CreateArticlePageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: '', component: SignInPageComponent },
+];
 
 @NgModule({
   declarations: [
@@ -22,8 +36,10 @@ import { ArticleListComponent } from './components/article-list/article-list.com
     ArticleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
