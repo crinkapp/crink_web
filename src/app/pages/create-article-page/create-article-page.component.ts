92a8f9da75df0759ec0ad8a3bf6a268683dadf9b
Eudrey <eudreybandelaria@gmail.com>
2020-04-18 17:47:50 +0200

    simplify style

--- src/app/pages/create-article-page/create-article-page.component.ts
+++ src/app/pages/create-article-page/create-article-page.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-article-page.component.scss']
 })
 export class CreateArticlePageComponent implements OnInit {
+  addedImage: boolean = false;
+  thumnnail = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChange(e: any) {
+    if (!this.addedImage) {
+      this.addedImage = true;
+    }
+    this.thumnnail = window.URL.createObjectURL(e);
+    console.log(this.thumnnail);
+    
   }
 
 }
