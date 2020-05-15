e017c1d75f6c85cab35cdad7271a997a28e02d49
Eudrey <eudreybandelaria@gmail.com>
2020-05-15 19:29:55 +0200

    added v.0 of diagnostic template

--- src/app/pages/diagnostic-page/diagnostic-page.component.ts
+++ src/app/pages/diagnostic-page/diagnostic-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+type Page = 'main' | 'one' | 'two' | 'three' | 'four' | 'five';
+
 @Component({
   selector: 'app-diagnostic-page',
   templateUrl: './diagnostic-page.component.html',
   styleUrls: ['./diagnostic-page.component.scss']
 })
 export class DiagnosticPageComponent implements OnInit {
+  page: Page = 'main';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goToPage(p:Page) {
+    this.page = p;
   }
 
 }
