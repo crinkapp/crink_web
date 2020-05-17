daa36929af0453cde6e36e3d5ba822aae9d23664
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-05-17 16:12:57 +0200

    replace newsletter section by contact section

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,44 +6,10 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  emailReg = RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$');
-  loading: boolean = false;
-  error: boolean | null = null;
-  sent: boolean = false;
-  isAccepted: boolean = false;
 
-  constructor(private service: UserService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onAccept(ev: boolean) {
-    this.isAccepted = ev;
-  }
-
-  onSubmit(email: string) {
-    this.loading = true;
-    if (this.emailReg.test(email)) {
-      // if email address is valid
-      this.service.sendEmail(email)
-        .subscribe(
-          () => {
-            // if sent successfully
-            this.error = false;
-            this.sent = true;
-            this.loading = false;
-          },
-          ), (err: any) => {
-            // if error in sending email
-            console.log(err);
-            this.error = true;
-            this.loading = false;
-        }
-    } else {
-      // if email address isn't valid
-      this.error = true;
-      this.loading = false;
-    }
   }
 
 }
