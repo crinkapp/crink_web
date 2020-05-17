e9782b006615b4bf381c33291003ec0f71895d13
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-05-18 00:39:03 +0200

    added unsubscribe newsletter page

--- src/app/pages/unsubscribe-page/unsubscribe-page.component.ts
+++ src/app/pages/unsubscribe-page/unsubscribe-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-unsubscribe-page',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./unsubscribe-page.component.scss']
 })
 export class UnsubscribePageComponent implements OnInit {
+  email: string = '';
+  emailReg = RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$');
+  loading: boolean = false;
+  error: boolean | null = null;
+  sent: boolean = false;
+  isAccepted: boolean = false;
 
-  constructor() { }
+  constructor(private user: UserService) { }
 
   ngOnInit() {
+  }
+
+  onAccept(ev: boolean) {
+    this.isAccepted = ev;
+  }
+
+  onSubmit() {
+    this.loading = true;
+    if (this.emailReg.test(this.email)) {
+      // if email address is valid
+      this.user.unsubscribe(this.email)
+        .subscribe(
+          () => {
+            // if sent successfully
+            this.error = false;
+            this.sent = true;
+            this.loading = false;
+          }
+        ), (err) => {
+          // if error in sending email
+          console.log(err);
+          this.error = true;
+          this.loading = false;
+        }
+    } else {
+        // if email address isn't valid
+        this.error = true;
+        this.loading = false;
+    }
   }
 
 }
