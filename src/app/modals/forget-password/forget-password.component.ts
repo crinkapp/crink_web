524b99cb54fed6c4e233c355e2aef7e8780f65ec
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-06-22 00:31:26 +0200

    added content for forgotten password modal

--- src/app/modals/forget-password/forget-password.component.ts
+++ src/app/modals/forget-password/forget-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,10 +7,23 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent implements OnInit {
+  emailReg = RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$');
+  email: string = '';
+  error: boolean = false;
 
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    if(!this.emailReg.test(this.email)) {
+      this.error = true;
+      console.log('error');
+    } else {
+      console.log(this.email);
+      this.activeModal.close();
+    }
   }
 
 }
